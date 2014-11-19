29cde87757a858d078e7e0328170e10afc30560c
Yusuke Nemoto <stooner.hoe@gmail.com>
2014-11-20 08:11:23 +0900

    Update app.js * remove debug stdout

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ AWS.config.update({
 	"region": _s.trim(process.env.AWS_REGION)
 });
 
-console.log("AWS_ACCESS_KEY_ID: " + process.env.AWS_ACCESS_KEY_ID);
-console.log("AWS_SECRET_ACCESS_KEY: " + process.env.AWS_SECRET_ACCESS_KEY);
-console.log("region: " + AWS.config.region);
-
 app.get('/', function(req, res){
 	var ec2 = new AWS.EC2();
 	var list = [];
